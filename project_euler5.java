021ab1ad4522b1aa86fa3d4e1447bc6715075aa4
Ben <benjamin_clayman@alumni.brown.edu>
2014-05-29 21:34:40 -0400

    solved #5

---
+++ project_euler5.java
@@ -0,0 +1,38 @@
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+class project_euler5 {
+
+	public int answer;
+	public static boolean answerFound = false;
+
+	public static boolean candidateDivisibleBy(int candidate, int divisor) {
+		return (candidate % divisor == 0);
+	}
+
+	public static boolean divisibleByAllTwenty(int candidate) {
+		boolean divisible = false;
+		for (int i = 2; i <= 20; i++) {
+			if (!candidateDivisibleBy(candidate, i)) {
+				break;
+			}
+			if (i == 20) {
+				divisible = true;
+				break;
+			}
+		}
+		return divisible;
+	}
+
+	public static void main(String[] args) {
+		int candidate = 20;
+		while (answerFound == false) {
+			if (divisibleByAllTwenty(candidate)) {
+				answerFound = true;
+				break;
+			}
+			candidate++;
+		}
+		System.out.println("The smallest positive number divisible by all numbers 1-20 is " + candidate);
+
+	}
+}
